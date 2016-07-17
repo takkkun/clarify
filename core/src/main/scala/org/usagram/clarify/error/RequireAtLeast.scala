7d3c31f07ed52dde44952013e01871086c06d43b
Takahiro Kondo <heartery@gmail.com>
2016-07-17 11:26:10 +0900

    Align to UpToAtMost

--- core/src/main/scala/org/usagram/clarify/error/RequireAtLeast.scala
+++ core/src/main/scala/org/usagram/clarify/error/RequireAtLeast.scala
@@ -2,10 +2,10 @@ package org.usagram.clarify.error
 
 import org.usagram.clarify.Tags
 
-case class RequireAtLeast(size: Int) extends Error {
-  def message(tags: Tags) = s"${tags.label getOrElse "(no label)"} requires at least $sizeAndUnit"
+case class RequireAtLeast(limit: Int) extends Error {
+  def message(tags: Tags) = s"${tags.label getOrElse "(no label)"} requires at least $quantity"
 
-  private def sizeAndUnit: String = size match {
+  private def quantity: String = limit match {
     case 1 => "1 item"
     case n => s"$n items"
   }
--- core/src/main/scala/org/usagram/clarify/error/TooLongString.scala
+++ core/src/main/scala/org/usagram/clarify/error/TooLongString.scala
@@ -3,5 +3,10 @@ package org.usagram.clarify.error
 import org.usagram.clarify.Tags
 
 case class TooLongString(limit: Int) extends Error {
-  def message(tags: Tags) = s"${tags.label getOrElse "(no label)"} is too long, is up to at most $limit characters"
+  def message(tags: Tags) = s"${tags.label getOrElse "(no label)"} is too long, is up to at most $quantity"
+
+  private def quantity: String = limit match {
+    case 1 => "1 character"
+    case n => s"$n characters"
+  }
 }
